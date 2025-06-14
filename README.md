d72d7b26a3a760b26b529eea16de7a5637bd7c15
kimymania <kimymania@gmail.com>
2025-06-14 23:42:19 +0900

    Modifications after feedback

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Todo CLI app
 
 Simple app to practice file I/O and error handling in Python
+
+This app:
+1. Loads to-do list from tasks.txt file
+2. User can view current to-do list
+3. Adding to the list automatically updates list
+4. Repeats process until user closes the program
--- todo.py
+++ todo.py
@@ -1,48 +1,79 @@
+import os
 import sys
-from typing import TextIO
+import time
 
 
 def main() -> None:
-    print("Todo list".center(20, "="))
-    file: TextIO = load_file()
-    display_tasks(file)
+    print(
+        f"\
+    {'=' * 10}\n\
+    To-Do list\n\
+    {'=' * 10}\n\n\
+    Main Menu: \n\
+    1. View To-Do List\n\
+    2. Add New Task\n\
+    3. Exit Program"
+    )
+    data = load_file()
     while True:
-        add_task(file)
+        menu_input = input("\nSelect a number to continue: ")
+        if menu_input == "1":
+            display_tasks(data)
+        elif menu_input == "2":
+            add_task(data)
+        elif menu_input == "3":
+            break
+        else:
+            print("Invalid option! Choose between 1, 2 and 3")
+
+    print("Goodbye!")
+    time.sleep(1)
+    sys.exit()
 
 
-def load_file() -> TextIO:
+def load_file(filename: str = "tasks.txt") -> list:
     """
-    If tasks.txt exists => load file
-    If it doesn't exist => create file and load
+    If tasks.txt doesn't exist => create new file / Else, move on
+    Open file => store in local memory for use
     """
+    todo_list = []
+    if not os.path.exists(filename):
+        try:
+            open(filename, "w", encoding="utf-8").close()
+        except PermissionError as e:
+            print(f"Not permitted to create new file: {e}")
+        except OSError as e:
+            print(f"OS-level failure: {e}")
+            sys.exit(1)
+
     try:
-        file = open("tasks.txt", "a+", encoding="utf-8")
-        print("File opened".center(20, "-"))
-    except Exception as e:
-        print(f"Encountered unknown error: {e}")
+        with open(filename, "r", encoding="utf-8") as f:
+            for line in f.readlines():
+                todo_list.append(
+                    line.strip()
+                )  # readlines() adds a newline at the end of each string by default
+    except OSError as e:
+        print(f"OS-level failure: {e}")
         sys.exit(1)
-    return file
+
+    return todo_list
 
 
-def display_tasks(file: TextIO) -> None:
-    print("Current tasks list: ")
-    file.seek(0)  # Move pointer to beginning of stream
-    lines: list[str] = file.readlines()
-    if not lines:
-        print("Tasks list is empty")
+def display_tasks(data: list[str]) -> None:
+    print("Current to-do list:")
+    if not data:
+        print("To-do list is empty")
     else:
-        for line in lines:
-            print(line, end="")
+        for d in data:
+            print(d)
 
 
-def add_task(file: TextIO) -> None:
+def add_task(data: list[str], filename: str = "tasks.txt") -> None:
     new_task: str = input("Add new task: ")
-    if new_task.lower() == "exit" or new_task.lower() == "quit":
-        file.close()
-        sys.exit()
-    else:
-        file.write(f"{new_task}\n")
-        display_tasks(file)
+    data.append(new_task)
+    with open(filename, "w", encoding="utf-8") as f:
+        for d in data:
+            f.write(f"{d}\n")
 
 
 if __name__ == "__main__":
